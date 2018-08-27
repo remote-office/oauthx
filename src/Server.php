0fcc90f36002fbfdb1c1ef2a18691d2e50092498
David Betgen <db@yes-co.nl>
2018-08-27 21:43:54 +0200

    Initial commit

---
+++ src/Server.php
@@ -0,0 +1,16 @@
+<?php
+
+  namespace OAuthX;
+
+  /**
+   * OAuthX Server class (implements OAuth2 protocol)
+   *
+   * @author David Betgen <d.betgen@remote-office.nl>
+   * @version 1.0
+   */
+  class Server
+  {
+
+  }
+
+?>
